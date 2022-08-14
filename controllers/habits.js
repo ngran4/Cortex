f0f6770a9e0e70a7ea2864c46a31e461ffbd671c
ngran4 <nicgranville@gmail.com>
2022-08-14 16:51:44 -0700

    new fn, create fn, new view, router paths

--- controllers/habits.js
+++ controllers/habits.js
@@ -3,6 +3,8 @@ const Profile = require('../models/profile');
 
 module.exports = {
     index,
+    new: newHabit,
+    create
 }
 
 async function index(req, res) {
@@ -16,4 +18,34 @@ async function index(req, res) {
     } catch(err){
         res.send(err)
     }
+};
+
+function newHabit(req, res) {
+    res.render('habits/new.ejs')
+    console.log(newHabit, '<- new Habit')
+};
+
+// async 
+function create(req, res) {
+    console.log(req.body)
+
+    Habit.create(req.body, function(err, habitDocCreated){
+        if(err){
+            console.log(err, '<- err in habits create controller')
+            // return res.render('/habits/new.ejs')
+        }
+        console.log(habitDocCreated, '<- habit created in db')
+        res.redirect('/habits')
+    })
+
+    // try {
+
+    // const habitDocCreated = await Habit.create(req.body)
+    // console.log(habitDocCreated, 'habit doc created in db')
+    // res.redirect('/habits/habits')
+    // // res.redirect(`habits/${habitDocCreated._id}`)
+    
+    // } catch(err){
+    //     res.send(err)
+    // }
 }
--- routes/habits.js
+++ routes/habits.js
@@ -4,5 +4,7 @@ const habitsController = require('../controllers/habits');
 const isLoggedIn = require('../config/auth');
 
 router.get('/', habitsController.index);
+router.get('/new', habitsController.new);
+router.post('/', habitsController.create);
 
 module.exports = router;
