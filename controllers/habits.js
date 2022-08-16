051970c58a7318a14c0d688dd1dc7a3b1f308e32
ngran4 <nicgranville@gmail.com>
2022-08-16 11:09:59 -0700

    change date fn

--- controllers/habits.js
+++ controllers/habits.js
@@ -66,8 +66,8 @@ async function show(req, res){
         const habitDoc = await Habit.findById(req.params.id)
         console.log(habitDoc, '<- habitDoc')
 
-        const streak = updateStreak(habitDoc)
-        console.log(streak, 'this is the streak')
+        // const streak = updateStreak(habitDoc)
+        // console.log(streak, 'this is the streak')
         
         res.render('habits/show', {
             habit: habitDoc,
@@ -107,53 +107,62 @@ async function updateHabit(req, res){
 
 
 // -------------- STREAK -------------- //
-function diffInDays(date1, date2) {
-    dt1 = new Date(date1);
-    dt2 = new Date(date2);
-    return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
-};
+// function diffInDays(date1, date2) {
+//     dt1 = new Date(date1);
+//     dt2 = new Date(date2);
+//     return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+// };
 
-function updateStreak(habit) {
-    let length = habit.habitLog.length
-    const sortedDesc = habit.habitLog.sort(
-        (objA, objB) => Number(objB.createdAt) - Number(objA.createdAt),
-    );
-    console.log(sortedDesc, 'sortedDesc fn')
+// function updateStreak(habit) {
+//     let length = habit.habitLog.length
+//     const sortedDesc = habit.habitLog.sort(
+//         (objA, objB) => Number(objB.createdAt) - Number(objA.createdAt),
+//     );
+//     console.log(sortedDesc, 'sortedDesc fn')
 
-    let today = new Date().toLocaleDateString();
-    let streak = 0;
-    if (today = sortedDesc[0].createdAt) {
-        streak = 1;
-    }
-    console.log(diffInDays(today, sortedDesc[0].createdAt));
-    if (today == sortedDesc[0].createdAt || diffInDays(today, sortedDesc[0].createdAt == 1)) {
-          for (let i = 0; i < length; i++) {
-            if(i== length - 1){
-                return streak
-            }
-          let diff = diffInDays(sortedDesc[i].createdAt, sortedDesc[i + 1].createdAt);
-          console.log(diff, 'this is the diff')
-        }}
-        //   if (diff == 1) {
-        //     streak += 1
-        //   } else if (diff > 1) {
-        //     return streak
-        //   }
-        // }
-        //   return streak
-        // } else {
-        //   return streak
-        // }
-};
+//     let today = new Date().toLocaleDateString();
+//     let streak = 0;
+//     if (today = sortedDesc[0].createdAt) {
+//         streak = 1;
+//     }
+//     console.log(diffInDays(today, sortedDesc[0].createdAt));
+//     if (today == sortedDesc[0].createdAt || diffInDays(today, sortedDesc[0].createdAt == 1)) {
+//         for (let i = 0; i < length; i++) {
+//             if(i== length - 1){
+//             return streak
+//             }
+
+//         let diff = diffInDays(sortedDesc[i].createdAt, sortedDesc[i + 1].createdAt);
+//         console.log(diff, 'this is the diff')
+//     }}
+
+//         // if (diff == 1) {
+//         // streak += 1
+//         // } else if (diff > 1) {
+//         // return streak
+//         // }
+//         // } else {
+//         //   return streak
+//         // }
+// };
 
 
-// loop through, count true
-// habitlog array -- set to empty array?
 
-// fin dmost recent, count back # trues
+// // - Order (habit log?) array from most recent -> least recent dates (desc)
+// const sortDateDesc = habit.habitLog.sort(a, b) => 
 
-// method = put
-// id, streak, habit
-// { habit: {
-//    streak: JSON.stringify(streak),
-//    habit_title: title }
+// - Define streak, set count = to 0
+
+// - Set today = to current date
+
+// - Make sure multiple logs in a day do not count towards streak:
+// 	- use .map to create array of dates, converted from createdAt timestamp for every log
+// 	- ?? remove duplicate dates? (.uniq) and form new array
+
+// - take new array, use .reduce (acc, date)
+// 	- starting value (acc?) = today
+// 	- define yesterday = date of day before accumulator
+// 	- IF statment:
+// 		- if date = yesterday OR = today
+// 		- streak count += 1 and acc = date 
+// - return streak count
